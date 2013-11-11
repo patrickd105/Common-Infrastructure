695b5798706f481dc5df188eb9bd58e37dcc26dc
Thomas Griffin <Thomas@Thomas-MacBook-Pro.local>
2013-11-10 21:13:51 -0600

    Modifying client to use the static interfaces

--- TestClient.java
+++ TestClient.java
@@ -9,23 +9,32 @@ public class TestClient {
 
 	public static void main (String args[]) throws Exception {
 	
+		/*
 		System.out.println("Making connection");
 		Socket s = new Socket("127.0.0.1" , 4444);
-		int option = 0;
+		
 		int currentID = 0;
 		
 		ObjectOutputStream objOut = new ObjectOutputStream(s.getOutputStream() );
 		ObjectInputStream objIn = new ObjectInputStream(s.getInputStream() );
 		
-		Message outMessage = null;
-		Message idAssignment = null;
+		
 
-		Scanner scan = new Scanner(System.in);
+		
 		
 		idAssignment = (Message) objIn.readObject();
 		currentID = ((RegistrationMessage) idAssignment).receivedID;
 		
 		System.out.println("Was given ID = "+currentID);
+		*/
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("before");
+		ClientInterface.init();
+		System.out.println("after");
+		Message outMessage = null;
+		Message idAssignment = null;
+		int option = 0;
 		
 		while( option != 3) {
 		
@@ -35,28 +44,31 @@ public class TestClient {
 			//Note: Only did one case for now out of laziness.
 			switch(option) {
 				case 1:
-					outMessage = new VideoMessage(currentID);
+					outMessage = new VideoMessage(0);
 					break;
 				case 3:
-					outMessage = new DeregisterMessage(currentID);
+					outMessage = new DeregisterMessage(0);
 					break;
 				default:
 					System.out.println("not an option");
 			
 			}
 			
-			if(outMessage != null) objOut.writeObject(outMessage);
+			if(outMessage != null) ClientInterface.sendMessage(outMessage);
 			
 			
 		};
 		
+		
+		ClientInterface.shutdown();
+		/*
 		Message closeConfirm = (Message) objIn.readObject();
 		if(  ((ConfirmationMessage) closeConfirm).confirmedMessageType == 4)
 			System.out.println("Exit confirmed");
 		else
 			System.out.println("Exit hosed up");
 			
-		s.close();		 
+		*/	 
 
 	}
 
