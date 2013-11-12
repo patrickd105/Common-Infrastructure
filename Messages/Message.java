b65e34fd573bb81e6851c056728d00482293c7eb
patrickd105 <patrickdunshee@usr2-246.dyn4.smu.edu>
2013-11-12 17:48:26 -0600

    Added comments to Messages package

--- Messages/Message.java
+++ Messages/Message.java
@@ -3,6 +3,11 @@ package Messages;
 import java.io.*;
 import java.util.Date;
 
+/**
+ *Message
+ *The base Message class designed to carry information between the client and server
+ @author Common Infrastructure
+ */
 public class Message implements Serializable  {
 
 	public int typeID;
@@ -11,6 +16,10 @@ public class Message implements Serializable  {
 	
 	//Other data members to be added later
 	
+    /**
+     *The constructor
+     *@param t: the Message type id. c: the clientID it came from
+     */
 	public Message(int t, int c) {
 		typeID = t;
 		clientID = c;
--- Messages/RegistrationMessage.java
+++ Messages/RegistrationMessage.java
@@ -1,10 +1,19 @@
 package Messages;
 
+/**
+ *RegistrationMessage
+ *The Message to register a client
+ *@author Common Infrastructure
+ */
 public class RegistrationMessage extends Message {
 
 	//For now, if -1 means a new client
 	public int receivedID;
 	
+    /**
+     *The constructor
+     *@param id: the clientID that it came from
+     */
 	public RegistrationMessage(int id) {
 		super(0,id);
 		receivedID = id;
