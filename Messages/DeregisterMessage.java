087da05aafda9b1c05bfb40a29de7a59e3ceeacc
Blossom Bologna <bbologna@smu.edu>
2013-11-12 18:15:33 -0600

    Added comments for DeregisterMessage

--- Messages/DeregisterMessage.java
+++ Messages/DeregisterMessage.java
@@ -1,9 +1,19 @@
 package Messages;
 
+/**
+ * DeregisterMessage
+ * Program extends Messages class -- a DeregisterMessage is the message telling the server to quit and disconnect the client
+ * @author Common Infrastructure
+ */
 public class DeregisterMessage extends Message {
 
 	//Any specific data?
 	
+	/**
+     * Constructor for DeregisterMessage
+     * @param client An int that contains the client ID
+     * @return No return value
+     */
 	public DeregisterMessage(int client) {
 		super(4, client);
 		
